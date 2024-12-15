b38401b83734a02ec2f86b5623c8f6bff5cb6210
Rafael Guerra <rafael_63_@hotmail.com>
2024-12-15 10:27:26 -0300

    Initial commit

---
+++ efeito.js
@@ -0,0 +1,16 @@
+function parar(){
+    const para = document.querySelectorAll(".lt").forEach(function(el){
+        el.style.animation = "none";
+       
+        
+    })
+
+}
+
+function iniciar(){
+    const iniciar = document.querySelectorAll(".lt").forEach(function(el){
+        el.style.animation ="rotate 8s linear infinite"
+         
+    })
+        
+    }
